a056616d15f23152820d480a5cfa11ac129f6074
酒井典昭 <sakainoriaki@sakainoriakinoMacBook-puro.local>
2019-09-22 22:09:49 +0900

    00007

---
+++ FirebaseClass7/FirebaseClass7/TopViewController.swift
@@ -0,0 +1,49 @@
+//
+//  TopViewController.swift
+//  FirebaseClass7
+//
+//  Created by 酒井典昭 on 2019/09/22.
+//  Copyright © 2019 典昭酒井. All rights reserved.
+//
+
+import UIKit
+import PGFramework
+
+
+// MARK: - Property
+class TopViewController: BaseViewController {
+    var userModel = UserModel()
+    
+}
+
+// MARK: - Life cycle
+extension TopViewController {
+    override func loadView() {
+        super.loadView()
+        
+        userModel.id = "1"
+        
+        UserModel.update(reqest: userModel) {
+            
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+}
+
+// MARK: - Protocol
+extension TopViewController {
+    
+}
+
+// MARK: - method
+extension TopViewController {
+    
+}
+
