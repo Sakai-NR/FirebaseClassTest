e72e9842da477e3371daa6e1d18015d130b5f0fc
酒井典昭 <sakainoriaki@sakainoriakinoMacBook-puro.local>
2019-09-22 22:28:31 +0900

    00008

---
+++ FirebaseClass8/FirebaseClass8/UserModel.swift
@@ -0,0 +1,33 @@
+//
+//  UserModel.swift
+//  FirebaseClass8
+//
+//  Created by 酒井典昭 on 2019/09/22.
+//  Copyright © 2019 典昭酒井. All rights reserved.
+//
+
+import UIKit
+import PGFramework
+import FirebaseDatabase
+
+// MARK: - Property
+class UserModel {
+    var id: String?
+    
+}
+
+// MARK: - Method
+extension UserModel {
+    static func setparamater(reqest: UserModel)-> [String: Any] {
+        var param = [String: Any]()
+        if let id = reqest.id{ param["id"] = id }
+        
+        return param
+    }
+    
+    static func update(reqest:UserModel,sacsess:@escaping() -> Void) {
+        let dbRef = Database.database().reference().child("users")
+        let param = setparamater(reqest: reqest)
+        dbRef.updateChildValues(param)
+    }
+}
